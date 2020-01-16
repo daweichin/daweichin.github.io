4d509c4fadb9dea10bd17bba915424adacab34c0
David Chin <dchin102@gmail.com>
2020-01-16 16:25:43 +1100

    removed modal img on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// about me text fadein effect
 function scrollAppear() {
   var text = document.querySelector(".about-list");
   var introPosition = text.getBoundingClientRect().top;
@@ -7,27 +8,9 @@ function scrollAppear() {
     text.classList.add("fade-in-down");
   }
 }
-
 window.addEventListener("scroll", scrollAppear);
 
-// modal logic for selecting correct modal
-// let modalBtn1 = document.querySelector(".modal-1");
-// var modalId = modalBtn1.classList[1];
-// let modal = document.getElementById(modalId);
-// let closeBtn1 = modal.querySelector(".close-btn");
-
-// modalBtn.onclick = function() {
-//   modal.style.display = "block";
-// };
-// closeBtn.onclick = function() {
-//   modal.style.display = "none";
-// };
-// window.onclick = function(e) {
-//   if (e.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
-
+// modal button logic
 let modalBtns = document.querySelectorAll(".button");
 for (var i = 0; i < modalBtns.length; i++) {
   modalBtns[i].addEventListener("click", function() {
