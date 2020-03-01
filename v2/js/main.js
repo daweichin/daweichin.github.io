af64a53c3202f63bb7932b3ee2de3ef7b8b70611
David Chin <dchin102@gmail.com>
2020-03-02 02:36:56 +1100

    v1 final version | v2 WIP

---
+++ v2/js/main.js
@@ -0,0 +1,12 @@
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  navigation: true,
+  scrollHorizontally: true,
+  recordHistory: false,
+  showActiveTooltip: true,
+  navigationTooltips: ["test", "secondslide"],
+  verticalCentered: false
+});
+
+fullpage_api.setScrollingSpeed(600);
